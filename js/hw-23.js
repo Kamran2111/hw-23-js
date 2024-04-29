2c1a4e523813e3549f8bb5738ea100f0a7530c52
Kama2111 <153067902+Kamran2111@users.noreply.github.com>
2024-04-29 20:51:11 +0400

    first commit

---
+++ js/hw-23.js
@@ -0,0 +1,36 @@
+// Задания: 4
+// const trafficLightBtn = document.querySelector(".traffic-light__btn");
+// const trafficLightRed = document.querySelector(".traffic-light__red");
+// const trafficLightYellow = document.querySelector(".traffic-light__yellow");
+// const trafficLightGreen = document.querySelector(".traffic-light__green");
+
+// let currentColor = "red";
+// trafficLightBtn.addEventListener("click", () => {
+//   if (currentColor === "red") {
+//     trafficLightRed.style.backgroundColor = "red";
+//     trafficLightYellow.style.backgroundColor = "#555";
+//     trafficLightGreen.style.backgroundColor = "#555";
+//     currentColor = "yellow";
+//   } else if (currentColor === "yellow") {
+//     trafficLightRed.style.backgroundColor = "#555";
+//     trafficLightYellow.style.backgroundColor = "yellow";
+//     trafficLightGreen.style.backgroundColor = "#555";
+//     currentColor = "green";
+//   } else {
+//     trafficLightRed.style.backgroundColor = "#555";
+//     trafficLightYellow.style.backgroundColor = "#555";
+//     trafficLightGreen.style.backgroundColor = "green";
+//     currentColor = "red";
+//   }
+// });
+// -----------------------------------------------------------------------
+// Задания: 5
+// const listBooks = document.querySelectorAll(".list__book");
+// listBooks.forEach((bookItem) => {
+//   bookItem.addEventListener("click", () => {
+//     listBooks.forEach((item) => {
+//       item.classList.remove("selected");
+//     });
+//     bookItem.classList.add("selected");
+//   });
+// });
